492ab6a7759d3edd52312fd225cf4a87db112e69
inviforce <saraswatdeepak0212@gmail.com>
2024-10-13 00:48:53 +0530

    hey how

--- Backend/model/user.js
+++ Backend/model/user.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose=require("mongoose");
 
 const userschema=new mongoose.Schema({
@@ -30,40 +29,3 @@ const userschema=new mongoose.Schema({
 
 const User=mongoose.model('User',userschema);
 module.exports=User;
-=======
-const mongoose = require("mongoose");
-
-const userschema = new mongoose.Schema({
-    voltage: {
-        type: Number,
-        required: true,
-    },
-    current: {
-        type: Number,
-        required: true,
-    },
-    power: {
-        type: Number,
-        required: true,
-    },
-    energy: {
-        type: Number,
-        required: true,
-    },
-    frequency: {
-        type: Number,
-        required: true,
-    },
-    power_f: {
-        type: Number,
-        required: true,
-    },
-    temperature: {
-        type: Number,
-        required: true,
-    }
-});
-
-const User = mongoose.model('User', userschema);
-module.exports = User;
->>>>>>> e84432e (connection of backend and frontend)
