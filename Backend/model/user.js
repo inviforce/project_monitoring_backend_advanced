d8bfaad1610c508a3c66685ca90b65969dcfb826
inviforce <saraswatdeepak0212@gmail.com>
2024-10-12 20:51:17 +0530

    connection of backend and frontend

--- Backend/model/user.js
+++ Backend/model/user.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const mongoose=require("mongoose");
 
 const userschema=new mongoose.Schema({
@@ -29,3 +30,40 @@ const userschema=new mongoose.Schema({
 
 const User=mongoose.model('User',userschema);
 module.exports=User;
+=======
+const mongoose = require("mongoose");
+
+const userschema = new mongoose.Schema({
+    voltage: {
+        type: Number,
+        required: true,
+    },
+    current: {
+        type: Number,
+        required: true,
+    },
+    power: {
+        type: Number,
+        required: true,
+    },
+    energy: {
+        type: Number,
+        required: true,
+    },
+    frequency: {
+        type: Number,
+        required: true,
+    },
+    power_f: {
+        type: Number,
+        required: true,
+    },
+    temperature: {
+        type: Number,
+        required: true,
+    }
+});
+
+const User = mongoose.model('User', userschema);
+module.exports = User;
+>>>>>>> e84432e (connection of backend and frontend)
