197d7c21a6d193659a7873ef40f95574c0ea25a7
inviforce <saraswatdeepak0212@gmail.com>
2025-01-07 23:39:05 +0530

    origin

--- Backend/information/mqtt.js
+++ Backend/information/mqtt.js
@@ -1,20 +1,20 @@
-// const holder={
-//     host: 'e800a45536b84764b7075bdc33165c5a.s1.eu.hivemq.cloud',
-//     port: 8883,
-//     username: 'hellomqtt',
-//     password: 'Hello@123',
-//     protocol: 'mqtts'
-// }
-//console.log(holder.host)
-
 const holder={
-    host: 'befc1420952d4ab8b4b9284843b122b9.s1.eu.hivemq.cloud',
+    host: 'e800a45536b84764b7075bdc33165c5a.s1.eu.hivemq.cloud',
     port: 8883,
-    username: 'someone',
-    password: 'Someone1',
+    username: 'hellomqtt',
+    password: 'Hello@123',
     protocol: 'mqtts'
 }
 console.log(holder.host)
+
+// const holder={
+//     host: 'befc1420952d4ab8b4b9284843b122b9.s1.eu.hivemq.cloud',
+//     port: 8883,
+//     username: 'someone',
+//     password: 'Someone1',
+//     protocol: 'mqtts'
+// }
+// console.log(holder.host)
 
 
 
